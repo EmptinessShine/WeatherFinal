ddef79308b87f75778ca23be8a0ee67464620739
Dias <diasabdulaziz0@gmail.com>
2024-12-22 00:12:16 +0500

    changed font

--- scripts/time.js
+++ scripts/time.js
@@ -6,7 +6,7 @@ function updateTime() {
     const hours = String(now.getHours()).padStart(2, '0');
     const minutes = String(now.getMinutes()).padStart(2, '0');
     const seconds = String(now.getSeconds()).padStart(2, '0');
-    currentTime.textContent = `Текущее время: ${hours}:${minutes}:${seconds}`;
+    currentTime.textContent = `Time: ${hours}:${minutes}:${seconds}`;
 }
 
 
